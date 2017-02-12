a16ad6a9acb51ff24504e42bf353a68781070eb6
Juho Jeong <aspectran@gmail.com>
2017-02-12 23:08:24 +0900

    aspectran 3.3.0 released!

--- app/run.bat
+++ app/run.bat
@@ -1,2 +1,5 @@
-@echo off
+@ECHO OFF
+ECHO -----------------=======================
+ECHO            ASPECTRAN CONSOLE
+ECHO =======================-----------------
 java -Dlog4j.configurationFile="file:/%CD%\log4j2.xml" -cp "lib/*" com.aspectran.console.Application
