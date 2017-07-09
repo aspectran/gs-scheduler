fcf0f909eaf716677da0314fabcdb12bdd08927c
Juho Jeong <aspectran@gmail.com>
2017-07-09 23:53:31 +0900

    Switch to SLF4J with Logback as primary logging framework

--- app/run.sh
+++ app/run.sh
@@ -2,4 +2,4 @@
 echo "-----------------======================="
 echo "          ASPECTRAN CONSOLE             "
 echo "=======================-----------------"
-java -Dlog4j.configurationFile="file://$PWD/log4j2.xml" -cp "lib/*" com.aspectran.console.AspectranConsole
+java -Dlogback.configurationFile="file://$PWD/logback.xml" -cp "lib/*" com.aspectran.console.AspectranConsole
