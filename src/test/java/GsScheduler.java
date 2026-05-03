763d7d2a0eb0feb2e049c41b4fc934465a2d4596
Juho Jeong <aspectran@gmail.com>
2026-05-03 14:52:05 +0900

    Update

--- src/test/java/GsScheduler.java
+++ src/test/java/GsScheduler.java
@@ -19,14 +19,14 @@ import com.aspectran.utils.ResourceUtils;
 import java.io.File;
 import java.io.IOException;
 
-import static com.aspectran.core.context.config.AspectranConfig.BASE_PATH_PROPERTY_NAME;
+import static com.aspectran.core.context.config.AspectranConfig.BASE_PATH_PROPERTY;
 
 public class GsScheduler {
 
     public static void main(String[] args) {
         try {
             File root = new File(ResourceUtils.getResourceAsFile(""), "../../app");
-            System.setProperty(BASE_PATH_PROPERTY_NAME, root.getCanonicalPath()); // for logback
+            System.setProperty(BASE_PATH_PROPERTY, root.getCanonicalPath()); // for logback
             JLineAspectranShell.main(new String[] { root.getCanonicalPath(), "config/aspectran-config.apon" });
         } catch (IOException e) {
             e.printStackTrace(System.err);
